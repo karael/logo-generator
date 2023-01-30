8aed18bf6faf369e25d2734f66e12b13d2ccfeed
karael <raphael.aupee@gmail.com>
2023-01-30 19:23:13 +0100

    feat: add prettier eslint and tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        strictPort: true,
+        hmr: { clientPort: 9001 },
+        host: true,
+        port: 9000,
+    },
 })
